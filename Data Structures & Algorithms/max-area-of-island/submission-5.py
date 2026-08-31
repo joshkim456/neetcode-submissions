e4ae0cb9950b23c6ffd9e3382fc37fedef547568
Josh Kim <108874541+joshkim456@users.noreply.github.com>
2026-08-31 20:09:43 +0900

    Add: max-area-of-island - submission-5

---
+++ Data Structures & Algorithms/max-area-of-island/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+
+        visited = [[False] * cols for _ in range(rows)]
+
+        maxArea = 0
+
+        def floodfill(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols: return 0
+            if visited[r][c]: return 0
+            if grid[r][c] == 0: return 0
+
+            visited[r][c] = True
+
+            return 1 + floodfill(r-1, c) + floodfill(r+1, c) + floodfill(r, c-1) + floodfill(r, c+1)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if not visited[r][c] and grid[r][c] == 1:
+                    maxArea = max(maxArea, floodfill(r, c))
+        
+        return maxArea
