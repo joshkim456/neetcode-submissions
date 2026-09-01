941fb9a44b665bb1e527bb16ca7e7e446d326726
Josh Kim <108874541+joshkim456@users.noreply.github.com>
2026-09-01 21:39:51 +0900

    Add: word-ladder - submission-7

---
+++ Data Structures & Algorithms/word-ladder/submission-7.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        minDepth = float('inf')
+
+        setList = set(wordList)
+
+        q = deque()
+        q.append(beginWord)
+        steps = 1
+
+        while q:
+            for _ in range(len(q)):
+                word = q.popleft()
+                
+                if word == endWord:
+                    return steps
+
+                for ch in "abcdefghijklmnopqrstuvwxyz":
+                    for i in range(len(word)):
+                        if word[:i] + ch + word[i+1:] in setList:
+                            setList.remove(word[:i] + ch + word[i+1:])
+                            q.append(word[:i] + ch + word[i+1:])
+            steps += 1
+
+        return 0
+            
